a42e645e8976469b68c86f11c59815023df4b4b9
Yuri Seabra <yuri97real@gmail.com>
2024-01-23 19:28:49 -0400

    add response manager

--- app/Controllers/HostController.php
+++ app/Controllers/HostController.php
@@ -2,11 +2,17 @@
 
 namespace App\Controllers;
 
+use Core\iResponse;
+
 class HostController
 {
-    public function index($request)
-    {}
+    public function index($request, iResponse $response)
+    {
+        return $response->view("hosts");
+    }
 
-    public function getHosts()
-    {}
+    public function getHosts($request, iResponse $response)
+    {
+        return $response->json([]);
+    }
 }
--- core/Response.php
+++ core/Response.php
@@ -2,10 +2,61 @@
 
 namespace Core;
 
-class Response
+interface iResponse
 {
-    public function __construct()
+    public function status(int $response_code);
+    public function redirect(string $route, array $query_params = []);
+    public function setTemplate($template);
+    public function view(string $view, array $data = []);
+    public function json(array $data);
+}
+
+class Response implements iResponse
+{
+    private $response_code = 200, $template = "main";
+
+    public function status(int $response_code)
     {
+        $this->response_code = $response_code;
+        return $this;
+    }
+
+    public function redirect(string $route, array $query_params = [])
+    {
+        $route = trim($route, "/");
+
+        $query_string = empty($query_params) ? "" : ("?" . http_build_query($query_params));
+        $redirect_link = BASE_URL . "/{$route}" . $query_string;
+
+        header("Location: {$redirect_link}");
+
+        die;
+    }
+
+    public function setTemplate($template)
+    {
+        $this->template = $template;
+        return $this;
+    }
+
+    public function view(string $view, array $data = [])
+    {
+        http_response_code($this->response_code);
+
+        extract($data);
+
+        $view = __DIR__."/../app/Views/pages/{$view}.php";
         
+        if( empty($this->template) ) {
+            require_once $view;
+        } else {
+            require_once __DIR__."/../app/Views/templates/{$this->template}.php";
+        }
+    }
+
+    public function json(array $data)
+    {
+        http_response_code($this->response_code);
+        echo json_encode($data);
     }
 }
